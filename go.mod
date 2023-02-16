46683f742891070d26b7e125444ff449bdaa7807
Hisham Galal <0xhgalal@gmail.com>
2023-02-15 21:12:20 -0500

    sample to be used with gnark-setup

---
+++ go.mod
@@ -0,0 +1,19 @@
+module sample
+
+go 1.19
+
+require (
+	github.com/consensys/gnark v0.7.0
+	github.com/consensys/gnark-crypto v0.7.0
+)
+
+require (
+	github.com/fxamacker/cbor/v2 v2.4.0 // indirect
+	github.com/mmcloughlin/addchain v0.4.0 // indirect
+	github.com/rs/zerolog v1.26.1 // indirect
+	github.com/x448/float16 v0.8.4 // indirect
+	golang.org/x/crypto v0.0.0-20220321153916-2c7772ba3064 // indirect
+	golang.org/x/sys v0.0.0-20220927170352-d9d178bc13c6 // indirect
+)
+
+replace github.com/consensys/gnark v0.7.0 => github.com/hsg88/gnark v0.0.1
---
+++ main.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"os"
+
+	"github.com/consensys/gnark-crypto/ecc"
+	"github.com/consensys/gnark/backend/groth16"
+	"github.com/consensys/gnark/frontend"
+	"github.com/consensys/gnark/frontend/cs/r1cs"
+	"github.com/consensys/gnark/qap"
+	"github.com/consensys/gnark/std/hash/mimc"
+)
+
+// Circuit defines a pre-image knowledge proof
+// mimc(secret preImage) = public hash
+type Circuit struct {
+	// struct tag on a variable is optional
+	// default uses variable name and secret visibility.
+	PreImage frontend.Variable
+	Hash     frontend.Variable `gnark:",public"`
+}
+
+// Define declares the circuit's constraints
+// Hash = mimc(PreImage)
+func (circuit *Circuit) Define(api frontend.API) error {
+	// hash function
+	mimc, _ := mimc.NewMiMC(api)
+
+	// specify constraints
+	// mimc(preImage) == hash
+	mimc.Write(circuit.PreImage)
+	api.AssertIsEqual(circuit.Hash, mimc.Sum())
+
+	return nil
+}
+
+func ReadPKVK() (groth16.ProvingKey, groth16.VerifyingKey) {
+	pk := groth16.NewProvingKey(ecc.BN254)
+	vk := groth16.NewVerifyingKey(ecc.BN254)
+	pkFile, _ := os.Open("pk")
+	defer pkFile.Close()
+
+	pk.ReadFrom(pkFile)
+
+	vkFile, _ := os.Open("vk")
+	defer vkFile.Close()
+	vk.ReadFrom(vkFile)
+	return pk, vk
+}
+
+func main() {
+	var myCircuit Circuit
+	r1cs, _ := frontend.Compile(ecc.BN254, r1cs.NewBuilder, &myCircuit)
+
+	/* --------- Run this once then use the output file with gnark-setup -------- */
+	var q qap.QAP
+	q.New(r1cs)
+	q.Save("qap")
+	/* -------------------------------------------------------------------------- */
+
+	/* ------- Copy pk and vk from gnark-setup then use the following code ------ */
+	pk, vk := ReadPKVK()
+
+	assignment := &Circuit{
+		PreImage: "16130099170765464552823636852555369511329944820189892919423002775646948828469",
+		Hash:     "8674594860895598770446879254410848023850744751986836044725552747672873438975",
+	}
+	witness, _ := frontend.NewWitness(assignment, ecc.BN254)
+	prf, err := groth16.Prove(r1cs, pk, witness)
+	if err != nil {
+		panic(err)
+	}
+	pubWitness, err := witness.Public()
+	if err != nil {
+		panic(err)
+	}
+	err = groth16.Verify(prf, vk, pubWitness)
+	if err != nil {
+		panic(err)
+	}
+
+}
